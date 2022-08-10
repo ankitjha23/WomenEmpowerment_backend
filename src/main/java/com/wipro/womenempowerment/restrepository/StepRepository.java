5d19f7d340bc4bb566e2429bc02d688823fd3a9a
ankitjha23 <ankitjha526@gmail.com>
2022-08-10 13:40:18 +0530

    project backend

---
+++ src/main/java/com/wipro/womenempowerment/restrepository/StepRepository.java
@@ -0,0 +1,10 @@
+package com.wipro.womenempowerment.restrepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wipro.womenempowerment.model.Step;
+
+public interface StepRepository extends JpaRepository<Step, Integer> {
+	
+	Step findById(int sId);
+}
